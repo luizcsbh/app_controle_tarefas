5b64c24792e691941f55607487d81e6cd3159cdc
luizc.lightbase <luizc.lightbase@sebraemg.com.br>
2022-07-13 19:25:13 -0300

    feat: Verifica se o usuario está logado

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tarefa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TarefaController extends Controller
 {
@@ -19,7 +20,30 @@ class TarefaController extends Controller
      */
     public function index()
     {
-        echo 'Chegamos aqui';
+        
+        $id = Auth::user()->id;
+        $name = Auth::user()->name;
+        $email = Auth::user()->email;
+        return "ID: $id | Nome: $name | E-mail: $email";
+        /*
+        if(Auth::check()) {
+            $id = Auth::user()->id;
+            $name = Auth::user()->name;
+            $email = Auth::user()->email;
+            return "ID: $id | Nome: $name | E-mail: $email";
+        } else {
+            return 'Você não está logado no Sistema';
+        }
+       
+        if(auth()->check()) {
+            $id = auth()->user()->id;
+            $name = auth()->user()->name;
+            $email = auth()->user()->email;
+            return "ID: $id | Nome: $name | E-mail: $email";
+        } else {
+            return 'Você não está logado no Sistema';
+        }
+        */
     }
 
     /**
